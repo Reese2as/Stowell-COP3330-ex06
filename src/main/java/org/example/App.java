671ba30847c8e44a86ff51882a0f3d9a7a473ef9
Reese2as <89660247+Reese2as@users.noreply.github.com>
2021-09-13 22:46:06 -0400

    Update App.java

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,15 +10,18 @@ public class App {
     public static void main(String[] args) {
         Scanner user_input = new Scanner(System.in);
         Date d=new Date();
-        int year =d.getYear();
+        int year = d.getYear();
         int age, age_r, t_left, year2;
+        
         System.out.print("What is your current age? ");
         age = user_input.nextInt();
         System.out.print("At what age would you like to retire ");
         age_r = user_input.nextInt();
-        int currentYear=year+1900;
+        
+        int currentYear= year+1900;
         t_left = (age_r-age);
         year2 = (currentYear + t_left);
+        
         System.out.println(String.format("You have %d years left until you can retire", t_left));
         System.out.println(String.format("Its %d, so you can retire in %d", currentYear, year2));
     }
